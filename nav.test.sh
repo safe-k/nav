9bc8c28bd39c2d32ee9d3c10473920682fdf5852
Seif Kamal <seif@tickx.co.uk>
2020-01-31 16:12:45 +0000

    Format scripts via Shfmt

--- nav.test.sh
+++ nav.test.sh
@@ -7,71 +7,71 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 FAILS=0
 PASSES=0
 
-assert_equal () {
-    if test "$1" == "$2"; then
-        ((++PASSES))
-    else
-        ((++FAILS))
-        echo "Failed to assert that $1 is equal to $2"
-    fi
+assert_equal() {
+  if test "$1" == "$2"; then
+    ((++PASSES))
+  else
+    ((++FAILS))
+    echo "Failed to assert that $1 is equal to $2"
+  fi
 }
 
-exec_nav () {
-    NO_ACTION_MESSAGES="true" FORMAT="false" nav "$@"
+exec_nav() {
+  NO_ACTION_MESSAGES="true" FORMAT="false" nav "$@"
 }
 
-run () {
-    # 1 Setup lab
-    LAB_DIR="$DIR/lab"
-    mkdir "$LAB_DIR"
-    cd "$LAB_DIR"
+run() {
+  # 1 Setup lab
+  LAB_DIR="$DIR/lab"
+  mkdir "$LAB_DIR"
+  cd "$LAB_DIR"
 
-    NAV="$LAB_DIR/nav"
-    cp "$DIR/nav" "$NAV"
+  NAV="$LAB_DIR/nav"
+  cp "$DIR/nav" "$NAV"
 
-    # shellcheck source=nav
-    source "$NAV"
+  # shellcheck source=nav
+  source "$NAV"
 
-    # 2 Define tests
+  # 2 Define tests
 
-    mkdir -p "$LAB_DIR/Someplace/Somewhere/Deep Inside"
+  mkdir -p "$LAB_DIR/Someplace/Somewhere/Deep Inside"
 
-    ## which
-    assert_equal "$(exec_nav which)" "$LAB_DIR"
+  ## which
+  assert_equal "$(exec_nav which)" "$LAB_DIR"
 
-    ## pin
-    exec_nav pin . lab
-    exec_nav pin ./Someplace/ place
-    exec_nav pin Someplace/Somewhere
-    exec_nav pin "$LAB_DIR/Someplace/Somewhere/Deep Inside" inside
+  ## pin
+  exec_nav pin . lab
+  exec_nav pin ./Someplace/ place
+  exec_nav pin Someplace/Somewhere
+  exec_nav pin "$LAB_DIR/Someplace/Somewhere/Deep Inside" inside
 
-    assert_equal "$(exec_nav pin invalid-dir dir)" "'$(pwd)/invalid-dir' is not a directory"
-    assert_equal "$(exec_nav pin . "£@invalid-name")" "Alias must only contain alphanumeric characters"
-    assert_equal "$(exec_nav pin ..)" "'..' could not be resolved"
+  assert_equal "$(exec_nav pin invalid-dir dir)" "'$(pwd)/invalid-dir' is not a directory"
+  assert_equal "$(exec_nav pin . "£@invalid-name")" "Alias must only contain alphanumeric characters"
+  assert_equal "$(exec_nav pin ..)" "'..' could not be resolved"
 
-    ## to
-    exec_nav to place
-    assert_equal "$(pwd)" "$LAB_DIR/Someplace"
+  ## to
+  exec_nav to place
+  assert_equal "$(pwd)" "$LAB_DIR/Someplace"
 
-    exec_nav to Somewhere
-    assert_equal "$(pwd)" "$LAB_DIR/Someplace/Somewhere"
+  exec_nav to Somewhere
+  assert_equal "$(pwd)" "$LAB_DIR/Someplace/Somewhere"
 
-    exec_nav to inside
-    assert_equal "$(pwd)" "$LAB_DIR/Someplace/Somewhere/Deep Inside"
+  exec_nav to inside
+  assert_equal "$(pwd)" "$LAB_DIR/Someplace/Somewhere/Deep Inside"
 
-    exec_nav to lab
-    assert_equal "$(pwd)" "$LAB_DIR"
+  exec_nav to lab
+  assert_equal "$(pwd)" "$LAB_DIR"
 
-    ## rm
-    exec_nav rm Somewhere
-    exec_nav rm inside
-    assert_equal "$(exec_nav to inside)" "No alias found matching 'inside'"
+  ## rm
+  exec_nav rm Somewhere
+  exec_nav rm inside
+  assert_equal "$(exec_nav to inside)" "No alias found matching 'inside'"
 
-    ## list
-    assert_equal "$(exec_nav list)" "$(echo -e "lab=$LAB_DIR\nplace=$LAB_DIR/Someplace")"
+  ## list
+  assert_equal "$(exec_nav list)" "$(echo -e "lab=$LAB_DIR\nplace=$LAB_DIR/Someplace")"
 
-    # 3 Destroy lab
-    rm -r "$LAB_DIR"
+  # 3 Destroy lab
+  rm -r "$LAB_DIR"
 }
 
 run
@@ -80,5 +80,5 @@ echo "$PASSES Successful"
 echo "$FAILS Failed"
 
 if [ ! "$FAILS" = 0 ]; then
-    exit 1
+  exit 1
 fi
